e688af145d851b49f64da1764a3e87992d5416a4
AzureByte <kenricdsouza@gmail.com>
2014-09-11 01:59:38 +0530

    Added smooth scrolling

---
+++ js/smoothscroll.js
@@ -0,0 +1,25 @@
+$(".navbar-collapse ul li a[href^='#'").on('click', function(e){
+	
+	e.preventDefault();
+
+	var hash = this.hash;
+
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top -50
+	}, 400, function(){
+		window.location.hash = hash;
+	});
+});
+
+$("a.navbar-brand[href^='#'").on('click', function(e){
+	
+	e.preventDefault();
+
+	var hash = this.hash;
+
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top -50
+	}, 250, function(){
+		window.location.hash = hash;
+	});
+});
